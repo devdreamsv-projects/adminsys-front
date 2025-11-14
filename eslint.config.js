29e796811de64a90c2dc796205e5c16a5a1f94db
Xavier Canjura <fernanxavi58@gmail.com>
2025-11-13 21:42:36 -0600

    🔧 chore: update eslint rules

--- eslint.config.js
+++ eslint.config.js
@@ -32,8 +32,48 @@ module.exports = tseslint.config(
           style: "kebab-case",
         },
       ],
+
+      // Angular best practices
+      "@angular-eslint/no-empty-lifecycle-method": "warn",
+      "@angular-eslint/prefer-on-push-component-change-detection": "warn",
+      "@angular-eslint/prefer-output-readonly": "warn",
+      "@angular-eslint/prefer-signals": "warn",
+      "@angular-eslint/prefer-standalone": "warn",
+
+      // typescript best practices
       "@typescript-eslint/no-explicit-any": "error",
+      "@typescript-eslint/array-type": ["error"],
+      "@typescript-eslint/consistent-indexed-object-style": "off",
+      "@typescript-eslint/consistent-type-assertions": "warn",
+      "@typescript-eslint/consistent-type-definitions": ["warn", "type"],
+      "@typescript-eslint/explicit-function-return-type": "error",
+      "@typescript-eslint/no-inferrable-types": "error",
       "@typescript-eslint/no-unsafe-assignment": "off",
+      "@typescript-eslint/no-unused-vars": "error",
+
+      // JavaScript best practices
+      "eqeqeq": "error",
+      "complexity": ["error", 20],
+      "guard-for-in": "error",
+      "max-classes-per-file": ["error", 1],
+      "max-lines": ["error", 400],
+      "no-bitwise": "error",
+      "no-console": ["error", { allow: ["warn", "error"] }],
+      "no-new-wrappers": "error",
+      "sort-imports": [
+        "error",
+        {
+          ignoreCase: true,
+          ignoreDeclarationSort: true,
+          allowSeparatedGroups: true,
+        }
+      ],
+
+      // Secutity
+      "no-eval": "error",
+      "no-implied-eval": "error",
+
+      // prettier rules
       "prettier/prettier": [
         "error",
         {
@@ -47,7 +87,42 @@ module.exports = tseslint.config(
     extends: [
       ...angular.configs.templateRecommended,
       ...angular.configs.templateAccessibility,
+      eslintPluginPrettierRecommended
     ],
-    rules: {},
+    rules: {
+
+      // Angular template best practices
+      "@angular-eslint/template/attributes-order": [
+        "error",
+        {
+          alphabetical: true,
+          order: [
+            'STRUCTURAL_DIRECTIVE', // deprecated, use @if and @for instead
+            'TEMPLATE_REFERENCE', // e.g. `<input #inputRef>`
+            'ATTRIBUTE_BINDING', // e.g. `<input required>`, `id="3"`
+            'INPUT_BINDING', // e.g. `[id]="3"`, `[attr.colspan]="colspan"`,
+            'TWO_WAY_BINDING', // e.g. `[(id)]="id"`,
+            'OUTPUT_BINDING', // e.g. `(idChange)="handleChange()"`,
+          ],
+        },
+      ],
+      '@angular-eslint/template/button-has-type': 'warn',
+      '@angular-eslint/template/cyclomatic-complexity': ['warn', { maxComplexity: 10 }],
+      '@angular-eslint/template/prefer-control-flow': 'error',
+      '@angular-eslint/template/prefer-ngsrc': 'warn',
+      '@angular-eslint/template/prefer-self-closing-tags': 'warn',
+      "@angular-eslint/template/no-duplicate-attributes": "warn",
+      "@angular-eslint/template/no-inline-styles": "warn",
+      "@angular-eslint/template/no-nested-tags": "error",
+      
+      // Prettier rules
+        "prettier/prettier": [
+          "error",
+          {
+            endOfLine: "auto",
+          }
+        ]
+    },
+
   }
 );
